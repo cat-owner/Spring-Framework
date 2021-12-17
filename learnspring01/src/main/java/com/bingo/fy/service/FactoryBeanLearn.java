dcbd630fb19b3c754aeca090d8094a7018f7980f
wangyuancun <wangyc5@yonghui.cn>
2021-12-17 13:15:18 +0800

    编译优化

--- learnspring01/src/main/java/com/bingo/fy/service/FactoryBeanLearn.java
+++ learnspring01/src/main/java/com/bingo/fy/service/FactoryBeanLearn.java
@@ -1,15 +1,15 @@
-package com.bingo.fy.service;
-
-import org.springframework.beans.factory.FactoryBean;
-
-public class FactoryBeanLearn implements FactoryBean {
-	@Override
-	public Object getObject() throws Exception {
-		return null;
-	}
-
-	@Override
-	public Class<?> getObjectType() {
-		return null;
-	}
-}
+//package com.bingo.fy.service;
+//
+//import org.springframework.beans.factory.FactoryBean;
+//
+//public class FactoryBeanLearn implements FactoryBean {
+//	@Override
+//	public Object getObject() throws Exception {
+//		return null;
+//	}
+//
+//	@Override
+//	public Class<?> getObjectType() {
+//		return null;
+//	}
+//}
